38332e5d8d153246498a526894ddc8ca9d636658
swapnilkale008 <swapnilkale008@gmail.com>
2023-03-15 15:09:06 +0530

    update

--- main.tf
+++ main.tf
@@ -1,47 +1,70 @@
-resource "aws_subnet" "main" {
-  vpc_id     = "data.aws_vpc.id"
-  cidr_block = "10.0.1.0/24"
 
-  tags = {
-    Name = "Main"
+data "aws_iam_policy_document" "lambda_policy" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents"
+    ]
+    resources = ["arn:aws:logs:*:*:*"]
   }
 }
 
-
-resource "aws_route_table" "example" {
-  vpc_id = "data.aws_vpc.id"
-
-  route {
-    cidr_block = "10.0.1.0/24"
-    gateway_id = data.aws_nat_gateway.id
-  }
-
-  tags = {
-    Name = "Main"
-  }
+data "aws_iam_role" "lambda_role" {
+  name = data.aws_iam_role.lambda.arn
 }
 
-resource "aws_iam_role_policy_attachment" "iam_role_policy_attachment_lambda_vpc_access_execution" {
-  role       = data.DevOps-Candidate-Lambda-Role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
+resource "aws_subnet" "private" {
+  vpc_id            = data.aws_vpc.id
+  cidr_block        = "10.0.1.0/24"
 }
 
-resource "aws_default_security_group" "default_security_group" {
-  vpc_id = data.vpc-0de2bfe0f5fc540e0.id
+resource "aws_route_table" "private" {
+  vpc_id = data.aws_vpc.vpc.id
+}
+
+resource "aws_route" "private_nat_gateway" {
+  route_table_id         = aws_route_table.private.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = data.aws_nat_gateway.nat.id
+}
+
+resource "aws_route_table_association" "private" {
+  subnet_id      = aws_subnet.private.id
+  route_table_id = aws_route_table.private.id
+}
+
+# create the Lambda function
+resource "aws_lambda_function" "example" {
+  filename         = "test.zip"
+  function_name    = "test_function"
+  role             = data.aws_iam_role.lambda.arn
+  handler          = "index.handler"
+  runtime          = "nodejs12.x"
+  source_code_hash = filebase64sha256("example.zip")
+}
+
+# create the security group
+resource "aws_security_group" "test" {
+  name_prefix = "security_group"
+  vpc_id      = data.aws_vpc.vpc.id
 
   ingress {
-    protocol  = -1
-    self      = true
     from_port = 0
-    to_port   = 0
+    to_port   = 65535
+    protocol  = "tcp"
+    cidr_blocks = [
+      "10.0.0.0/16"
+    ]
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-
+    from_port = 0
+    to_port   = 65535
+    protocol  = "tcp"
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
   }
-
 }
