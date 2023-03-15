8de5ca668720339c9b7e1ae141ece7195ef873b4
swapnilkale008 <swapnilkale008@gmail.com>
2023-03-15 16:17:22 +0530

    Update my-function.py

--- my-function.py
+++ my-function.py
@@ -13,3 +13,36 @@ def lambda_handler(event, context):
     response = requests.post(url, headers=headers, json=payload)
     print(response.status_code)
     print(response.text)
+   
+
+
+pipeline {
+    agent any
+
+    stages {
+        stage('Invoke API Endpoint') {
+            steps {
+                sh '''
+                    # Payload data
+                    PAYLOAD='{
+                        "subnet_id": "<Your Private Subnet ID>",
+                        "name": "<Your Full Name>",
+                        "email": "<Your Email Address>"
+                    }'
+
+                    # Security header
+                    HEADER='X-Siemens-Auth:test'
+
+                    # API endpoint
+                    URL='https://2xfhzfbt31.execute-api.eu-west-1.amazonaws.com/candidate-email_serverless_lambda_stage/data'
+
+                    # Invoke the API endpoint
+                    RESPONSE=$(curl -sS -X POST -H "${HEADER}" -H "Content-Type: application/json" -d "${PAYLOAD}" "${URL}")
+
+                    # Print the response
+                    echo ${RESPONSE}
+                '''
+            }
+        }
+    }
+}
