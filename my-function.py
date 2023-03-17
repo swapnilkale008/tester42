6bd9b278129cb1374a079b1b6e40b74056791fc5
swapnilkale008 <swapnilkale008@gmail.com>
2023-03-17 14:53:22 +0530

    Update my-function.py

--- my-function.py
+++ my-function.py
@@ -1,17 +1,25 @@
+import json
+import boto3
 import requests
 
 def lambda_handler(event, context):
+    # Define the payload
     payload = {
-        "subnet_id": "10.0.1.0/24",
+        "subnet_id": "10.0.1.0/16",
         "name": "swapnil kale",
-        "email": "swapnilkale2504@gmail.com"
+        "email": "swapnilkale008@gmail.com"
     }
-    headers = {
-        "X-Siemens-Auth": "test"
-    }
-    url = "https://2xfhzfbt31.execute-api.eu-west-1.amazonaws.com/candidate-email_serverless_lambda_stage/data"
-    response = requests.post(url, headers=headers, json=payload)
+    
+    # Define the header
+    headers = {'X-Siemens-Auth': 'test'}
+    
+    # Make the request
+    response = requests.post('https://2xfhzfbt31.execute-api.eu-west-1.amazonaws.com/candidate-email_serverless_lambda_stage/data', data=json.dumps(payload), headers=headers)
+
+    # Log the response status code
     print(response.status_code)
-    print(response.text)
 
-
+    return {
+        'statusCode': response.status_code,
+        'body': json.dumps('Hello from Lambda!')
+    }
