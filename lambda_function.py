8730ad1fa2b00c75f3c5afbc6678cf4deba3f356
swapnilkale008 <swapnilkale008@gmail.com>
2023-03-17 15:48:32 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -5,10 +5,10 @@ import requests
 def lambda_handler(event, context):
     # Define the payload
     payload = {
-        "subnet_id": "10.0.1.0/24",
-        "name": "swapnil kale",
-        "email": "swapnilkale008@gmail.com"
-    }
+    "subnet_id": "10.0.1.0/24",
+    "name": "swapnil kale",
+    "email": "swapnilkale008@gmail.com"
+}
     
     # Define the header
     headers = {'X-Siemens-Auth': 'test'}
