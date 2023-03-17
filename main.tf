fd77fc9bd1cbede5213e0aa5f166e4f41c74fb57
swapnilkale008 <swapnilkale008@gmail.com>
2023-03-17 14:52:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,5 @@
-
 resource "aws_subnet" "private" {
-  vpc_id            = data.aws_vpc.id
+  vpc_id            = data.aws_vpc.vpc.id
   cidr_block        = "10.0.1.0/24"
 }
 
@@ -8,10 +7,10 @@ resource "aws_route_table" "private" {
   vpc_id = data.aws_vpc.vpc.id
 }
 
-resource "aws_route" "private_nat_gateway" {
-  route_table_id         = aws_route_table.private.id
-  destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = data.aws_nat_gateway.nat.id
+resource "aws_route" "private_nat" {
+  route_table_id            = aws_route_table.private.id
+  destination_cidr_block    = "0.0.0.0/0"
+  nat_gateway_id            = data.aws_nat_gateway.nat.id
 }
 
 resource "aws_route_table_association" "private" {
@@ -19,51 +18,18 @@ resource "aws_route_table_association" "private" {
   route_table_id = aws_route_table.private.id
 }
 
-# create the Lambda function
-resource "aws_lambda_function" "test" {
-  filename         = "test.zip"
-  function_name    = "test_function"
+
+resource "aws_lambda_function" "example_lambda" {
+  filename         = "lambda_function_payload.zip"
+  function_name    = "example_lambda"
   role             = data.aws_iam_role.lambda.arn
-  handler          = "index.handler"
-  runtime          = "nodejs12.x"
-  source_code_hash = filebase64sha256("test.zip")
-}
-
-resource "aws_security_group" "test" {
-  name_prefix = "security_group"
-  vpc_id      = data.aws_vpc.vpc.id
-
-  ingress {
-    from_port = 0
-    to_port   = 65535
-    protocol  = "tcp"
-    cidr_blocks = [
-      "10.0.0.0/16"
-    ]
-  }
-
-  egress {
-    from_port = 0
-    to_port   = 65535
-    protocol  = "tcp"
-    cidr_blocks = [
-      "0.0.0.0/0"
-    ]
+  handler          = "lambda_function.lambda_handler"
+  runtime          = "python3.8"
+  timeout          = 30
+  memory_size      = 128
+  vpc_config {
+    subnet_ids = [aws_subnet.private.id]
+    security_group_ids = [aws_security_group.lambda.id]
   }
 }
 
-data "aws_iam_policy_document" "lambda_policy" {
-  statement {
-    effect = "Allow"
-    actions = [
-      "logs:CreateLogGroup",
-      "logs:CreateLogStream",
-      "logs:PutLogEvents"
-    ]
-    resources = ["arn:aws:logs:*:*:*"]
-  }
-}
-
-data "aws_iam_role" "lambda_role" {
-  name = data.aws_iam_role.lambda.arn
-}
